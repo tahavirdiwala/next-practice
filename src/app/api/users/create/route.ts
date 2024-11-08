e5805090c2460b02b94219b9405171a176485f14
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-08 19:01:43 +0530

    add role while creating user

--- src/app/api/users/create/route.ts
+++ src/app/api/users/create/route.ts
@@ -1,6 +1,7 @@
 import connectDb from "@/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/user.model";
+import Role from "@/models/role.model";
 
 await connectDb();
 
@@ -8,6 +9,18 @@ export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
     const user = new User({ ...body });
+
+    const role = await Role.findOne({ _id: body.role });
+
+    if (role) {
+      role.users.push(user._id);
+      await role.save();
+    } else {
+      NextResponse.json({
+        statusCode: 401,
+        message: "Role does not exist",
+      });
+    }
 
     await user.save();
     return NextResponse.json({
