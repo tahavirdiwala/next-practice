6b93f3d7fdf99e987c766458fdb1972331ffb67f
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-12 17:25:38 +0530

    ..

--- src/app/(client)/roles/page.tsx
+++ src/app/(client)/roles/page.tsx
@@ -3,7 +3,7 @@ import { trpc } from "@/utils/providers/queryClientProvider";
 import RoleList from "./_components/roleList";
 
 const Roles = () => {
-  const getRoles = trpc["get-roles"].useQuery();
+  const getRoles = trpc["get-roles"].useQuery(); // every type will automatically infer from backend due to trpc
   return getRoles?.data?.data?.map((item) => (
     <RoleList key={item._id} {...item} />
   ));
