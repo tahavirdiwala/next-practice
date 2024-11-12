f5c08ac89b6d8d60f4c392c1a2c16e3ee62c7512
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-12 18:54:37 +0530

    commenting

--- src/app/(client)/roles/_components/rolesWrapper.tsx
+++ src/app/(client)/roles/_components/rolesWrapper.tsx
@@ -5,8 +5,9 @@ import RoleList from "./roleList";
 
 const RolesWrapper = () => {
   const getRoles = trpc["get-roles"].useQuery(void 0, {
-    gcTime: 0, // for not cachetime
+    gcTime: 0, // for removing default not cachetime
   });
+
   return getRoles?.data?.data?.map((item) => (
     <RoleList key={item._id} {...item} />
   ));
