c83722c49aa0f5b207a2e1d6b8e086cd3fb5033f
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-14 09:35:11 +0530

    fix typo

--- src/app/(client)/roles/_components/roleList.tsx
+++ src/app/(client)/roles/_components/roleList.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React from "react";
 
-const RoleList = (props: Partial<{ _id: string; role: string }>) => {
+const RoleList = (props: { _id?: string; role: string }) => {
   return (
     <li>
       <Link href={`roles/${props._id}`}>{props.role}</Link>
