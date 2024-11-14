d32cac652bbfe0950d665d096ab5c877b14cb53c
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-14 09:19:25 +0530

    prettier

--- src/app/(client)/roles/[id]/page.tsx
+++ src/app/(client)/roles/[id]/page.tsx
@@ -1,15 +1,11 @@
-"use client"
+"use client";
 import { trpc } from "@/utils/providers/queryClientProvider";
 import { use } from "react";
 
-const RoleById =  ({ params }: { params: Promise<{ id: string }> }) => {
- const temp = use(params);
- const role = trpc.roles.get.useQuery({id: temp.id})
-  return <>
-  {
-    JSON.stringify(role?.data?.data, null)
-  }
-  </>
+const RoleById = ({ params }: { params: Promise<{ id: string }> }) => {
+  const temp = use(params);
+  const role = trpc.roles.get.useQuery({ id: temp.id });
+  return JSON.stringify(role?.data?.data);
 };
 
 export default RoleById;
