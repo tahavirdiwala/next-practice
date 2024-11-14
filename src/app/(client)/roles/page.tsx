f39ae333e9fb15f4e5f94068b27ff42d3cc40e06
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-14 18:45:31 +0530

    fix build error

--- src/app/(client)/roles/page.tsx
+++ src/app/(client)/roles/page.tsx
@@ -1,6 +1,9 @@
 import { Suspense } from "react";
 import RolesWrapper from "./_components/rolesWrapper";
 
+export const dynamic = "force-dynamic";
+// export const fetchCache = "force-no-store";
+
 const Roles = () => {
   return (
     <Suspense fallback={<p>Loading..</p>}>
--- src/app/(server)/api/users/route.ts
+++ src/app/(server)/api/users/route.ts
@@ -1,22 +1,20 @@
 import connectDb from "@/dbConfig";
-import { NextRequest } from "next/server";
 import { RESPONSE_MESSAGE as MESSAGE } from "@/app/lib/constant";
 import userService from "../../_services/user.service";
 import { StatusCodes } from "http-status-codes";
 import commonDecorators from "../../_common";
-import { UserInterFace } from "@/types/user";
 
 await connectDb();
 const { responser } = commonDecorators;
 
-async function POST(request: UserInterFace) {
-  try {
-    await userService.add(request);
-    return responser(MESSAGE.users.add, StatusCodes.CREATED);
-  } catch (error) {
-    return responser(`${error}`, StatusCodes.BAD_REQUEST);
-  }
-}
+// async function POST(request: NextRequest) {
+//   try {
+//     await userService.add(request);
+//     return responser(MESSAGE.users.add, StatusCodes.CREATED);
+//   } catch (error) {
+//     return responser(`${error}`, StatusCodes.BAD_REQUEST);
+//   }
+// }
 
 async function GET() {
   try {
@@ -27,4 +25,4 @@ async function GET() {
   }
 }
 
-export { POST, GET };
+export { GET };
