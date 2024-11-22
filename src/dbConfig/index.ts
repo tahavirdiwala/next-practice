eacf7793a913f20ecd12b5ea7ed1e88bf5d6549a
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-22 17:59:34 +0530

    checking for testing purpose

--- src/dbConfig/index.ts
+++ src/dbConfig/index.ts
@@ -2,7 +2,10 @@ import { connect } from "mongoose";
 
 export default async function connectDb() {
   try {
-    await connect(process.env.MONGO_URL!).then(() => {
+    // await connect(process.env.MONGO_URL!).then(() => {
+    await connect(
+      `mongodb+srv://taha:ILeKvx6neUsLMm1n@cluster0.g4yzn.mongodb.net/learning-next`
+    ).then(() => {
       console.log("database connected");
     });
   } catch (error) {
