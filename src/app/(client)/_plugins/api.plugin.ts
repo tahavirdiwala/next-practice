9791248a54df12933f34fd17cc553ba744ed7db9
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-26 14:57:27 +0530

    adding post decorator

--- src/app/(client)/_plugins/api.plugin.ts
+++ src/app/(client)/_plugins/api.plugin.ts
@@ -4,9 +4,18 @@ const API = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
+API.interceptors.request.use((response) => {
+  console.log("response", response);
+  return response;
+});
+
 const GET = async <T>(url: string, config?: AxiosRequestConfig) => {
   return API.get<ApiResponse<T>>(url, config).then((res) => res.data);
 };
 
-export { GET };
+const POST = async <T>(url: string, payload: object) => {
+  return API.post<ApiResponse<T>>(url, payload).then((res) => res.data);
+};
+
+export { POST, GET };
 export default API;
