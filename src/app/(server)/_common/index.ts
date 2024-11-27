c7c2db63209790d157156b7cf9cbba8d457dcc24
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-11-27 16:39:08 +0530

    add error handling message

--- src/app/(server)/_common/index.ts
+++ src/app/(server)/_common/index.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from "next/server";
 
 class CommonDecorators {
-  responser<T>(message: string | Error, statusCode: number, data?: T) {
-    return NextResponse.json({ message, statusCode, data });
+  responser<T>(dispatch: string | Error, statusCode: number, data?: T) {
+    if (dispatch.hasOwnProperty("message"))
+      dispatch = dispatch["message" as keyof typeof dispatch];
+
+    return NextResponse.json({ message: dispatch, statusCode, data });
   }
 }
 
